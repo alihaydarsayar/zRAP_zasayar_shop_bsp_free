6a1140e61d1fbeaf6a23b493123a30ea2b928697
Ali Haydar Sayar <97897666+alihaydarsayar@users.noreply.github.com>
2026-02-10 16:39:38 +0300

    fisrt

---
+++ webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+QUnit.config.autostart = false;
+
+sap.ui.require(["com/asayar/zasayarshopbspfree/test/integration/AllJourneys"
+], function () {
+	QUnit.start();
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"com/asayar/zasayarshopbspfree/test/unit/controller/Main.controller"
+], function () {
+	"use strict";
+});
